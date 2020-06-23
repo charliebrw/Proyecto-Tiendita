3fff2e4d4e3a9bd4408e3f03a8ff36990cacb5e4
Carlos Taipe <ctaipecasas@gmail.com>
2020-06-23 09:16:10 +0200

    Pepi mongol

---
+++ Incremento 2/Proyecto/Proyecto/Controllers/CategoríasController.cs
@@ -0,0 +1,97 @@
+﻿using Proyecto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto.Controllers
+{
+    public class CategoríasController : Controller
+    {
+        EdicasEntities1 db = new EdicasEntities1();
+        // GET: Categorías
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Categorías/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Categorías/Create
+        public ActionResult Create(int? idproducto)
+        {
+            return View();
+        }
+
+        // POST: Categorías/Create
+        [HttpPost]
+        public ActionResult Create(Categorias categoria,int? idproducto)
+        {
+            try
+            {
+                db.sp_A_Categoria(categoria.catNombre);
+                if(idproducto == null)
+                {
+                    return RedirectToAction("Create", "Productos");
+                }
+                else
+                {
+                    return RedirectToAction("Edit", "Productos", routeValues: new { id = idproducto});
+                }  
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Categorías/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Categorías/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Categorías/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Categorías/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Incremento 2/Proyecto/Proyecto/Controllers/RolesController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Proyecto.Models;
+
+namespace Proyecto.Controllers
+{
+    public class RolesController : Controller
+    {
+        ApplicationDbContext app = new ApplicationDbContext();
+        EdicasEntities1 db = new EdicasEntities1();
+        // GET: Roles
+        
+        public ActionResult Index()
+        {
+            var data = db.sp_C_Usuarios_Roles().ToList();
+            return View(data);
+        }
+
+        // POST: Roles/Edit/5
+        [HttpPost]
+        public ActionResult CambiarRol(string id)
+        {
+            
+
+                    var roleManager = new RoleManager<IdentityRole>
+                        (new RoleStore<IdentityRole>(app));
+                    var userManager = new UserManager<ApplicationUser>
+                           (new UserStore<ApplicationUser>(app));
+
+                    var UsuarioRol1 = userManager.IsInRole(id, "Administrador");
+                    if(UsuarioRol1 == true)
+                    {
+                        var resultado = userManager.AddToRole(id, "Vendedor");
+                        resultado = userManager.RemoveFromRole(id, "Administrador");
+                    }
+                    else
+                    {
+                        var resultado = userManager.AddToRole(id, "Administrador");
+                        resultado = userManager.RemoveFromRole(id, "Vendedor");
+                    } 
+                
+            return RedirectToAction("Index");
+
+        }
+
+        // GET: Roles/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
